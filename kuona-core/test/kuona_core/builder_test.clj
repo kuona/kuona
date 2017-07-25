01386b3d9990de83b0606e9b62e1bbf35777309f
Graham Brooks <graham@grahambrooks.com>
2017-07-25 08:16:15 -0600

    Updated builder tests and layout

--- kuona-core/test/kuona_core/builder_test.clj
+++ kuona-core/test/kuona_core/builder_test.clj
@@ -14,38 +14,42 @@
              ((build-tool "/some/path/makefile" "makefile") "/some/path/makefile") => {:builder "Make" :path "makefile"})
        (fact "handles rakefiles"
              ((build-tool "/some/path/Rakefile" "Rakefile") "/some/path/Rakefile") => {:builder "Rake" :path "Rakefile"})
-       (fact "handles maven"
+       (fact "handles maven and calls analyser"
              ((build-tool "/some/path/pom.xml" "pom.xml") "/some/path/pom.xml") => {:artifact {:artifactId "kuona-dashboard"
-                                                                                     :groupId    "kuona"
-                                                                                     :name       "Kuona analytics for software development teams"
-                                                                                     :version    "0.1"}
-                                                                          :builder  "Maven" :path "pom.xml"}
-             (provided (maven/load-pom-file "/some/path/pom.xml") => (xml/parse-str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">
-    <modelVersion>4.0.0</modelVersion>
-    <groupId>kuona</groupId>
-    <artifactId>kuona-dashboard</artifactId>
-    <packaging>pom</packaging>
-    <name>Kuona analytics for software development teams</name>
-    <version>0.1</version>
-</project>"))
-             )
+                                                                                               :groupId    "kuona"
+                                                                                               :name       "Kuona analytics for software development teams"
+                                                                                               :version    "0.1"}
+                                                                                    :builder  "Maven" :path "pom.xml"}
+
+             
+             (provided (maven/analyse-pom-file "/some/path/pom.xml") => {:artifact {:artifactId "kuona-dashboard"
+                                                                                    :groupId    "kuona"
+                                                                                    :name       "Kuona analytics for software development teams"
+                                                                                    :version    "0.1"}}))
        (fact "recognises leiningen"
              ((build-tool "/fo/project.clj" "/fo/project.clj") "/fo/project.clj") => {:builder "Leiningen" :path "/fo/project.clj"}) )
 
 
 (facts "about project scanning"
-       (collect-builder-metrics ".") =>  [{:artifact {:artifactId "kuona-core"
-                                                      :groupId "kuona-core"
-                                                      :name "kuona-core"
-                                                      :version "0.0.2"}
-                                           :builder "Maven"
-                                           :path "/pom.xml"}
+       (collect-builder-metrics ".") =>  [{:artifact {:artifactId    "kuona-core"
+                                                      :groupId       "kuona-core"
+                                                      :name          "kuona-core"
+                                                      :version       "0.0.2"
+                                                      :description   "Kuona core library"
+                                                      :inceptionYear nil
+                                                      :packaging     "jar"
+                                                      :url           "http://example.com/FIXME"}
+                                           :builder  "Maven"
+                                           :path     "/pom.xml"}
                                           {:builder "Leiningen"
-                                           :path "/project.clj"}
-                                          {:artifact {:artifactId "kuona-dashboard"
-                                                      :groupId "kuona"
-                                                      :name "Kuona analytics for software development teams"
-                                                      :version "0.1"}
-                                           :builder "Maven"
-                                           :path "/test/pom.xml"}])
+                                           :path    "/project.clj"}
+                                          {:artifact {:artifactId    "kuona-dashboard"
+                                                      :groupId       "kuona"
+                                                      :name          "Kuona analytics for software development teams"
+                                                      :version       "0.1"
+                                                      :description   nil
+                                                      :inceptionYear nil
+                                                      :packaging     "pom"
+                                                      :url           nil}
+                                           :builder  "Maven"
+                                           :path     "/test/pom.xml"}])
