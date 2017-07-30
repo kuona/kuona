5b82bffd6a91afa7464bf8ce8ff1ad8edbf29d60
Graham Brooks <graham@grahambrooks.com>
2017-07-29 19:21:02 -0600

    Added a module build technology chart to the repositories dashboard

--- dashboard/src/js/repositories.js
+++ dashboard/src/js/repositories.js
@@ -1,23 +1,50 @@
-var kuona_repositories = angular.module('kuona.repositories', [
-    'oc.lazyLoad',                  // ocLazyLoad
-    'ui.bootstrap',                 // Ui Bootstrap
-    'ngResource'
+var kuonaRepositories = angular.module('kuona.repositories', [
+    'ui.bootstrap'
 ]);
 
 
+kuonaRepositories.directive('kuonaBarChart', function() {
+  console.log('Initializing directive');
+  return {
+    restrict: 'E',
+    template: '<canvas class="build-tool-canvas" height="140"></canvas>',
+    scope: {
+      data: '='
+//      title: '=',
+//      description: '='
+    },
+    link: function (scope, element, attrs) {
+      barChart(d3.select(element[0]).select('.buid-tool-canvas'), scope.title, scope.description, scope.data);
+    }
+  };
+});
 
 function RepositoriesController($scope, $http) {
   this.helloText = 'Welcome to Kuona';
   this.descriptionText = 'Use the navigation to look around :)';
+
   $scope.currentDate = new Date();
   $scope.repository_count = "[loading]";
   $scope.vcs_count = "[loading]";
   $scope.repositoriesFound = [];
-  
+  $scope.buildTools = []
+
+  $http.get('/api/build/tools').then(function(res) {
+    $scope.buildTools = [];
+    var data = res.data.buckets;
+
+    var colorIndex = 0;
+    for (var k in data) {
+      var item = data[k];
+      $scope.buildTools.push({"label": item.key, "color": colors[colorIndex++], "value": item.doc_count });
+    }
+    barChart(document.getElementById("buildToolCanvas"), "Module Count", "Build tool counts for identified modules", $scope.buildTools);
+  });
+
   $http.get('/api/repositories/count').then(function(res) {
     $scope.repository_count = res.data.count;
   });
-  
+
   $http.get('/api/metrics/vcs/count').then(function(res) {
     $scope.vcs_count = res.data.count;
   });
@@ -26,14 +53,15 @@ function RepositoriesController($scope, $http) {
     $scope.code_metric_count = res.data.count;
   });
 
+
   $scope.repoSearch = function(term) {
-    console.log("Searching for " + term);
     $http.get("/api/repositories?search=" + term).then(function(res){
       $scope.repositoriesFound = res.data;
     });
-  }
+  };
 
   $scope.repoSearch("");
-};
+}
 
-kuona_repositories.controller('RepositoriesController',['$scope', '$http', RepositoriesController]);
+kuonaRepositories.controller('RepositoriesController',['$scope', '$http', RepositoriesController]);
+
--- dashboard/src/js/snapshot.js
+++ dashboard/src/js/snapshot.js
@@ -1,190 +1,8 @@
-var kuona_snapshot = angular.module('kuona.snapshot', [
-  'angular-websocket',
-  'ui.bootstrap'                 // Ui Bootstrap
+var kuonaSnapshot = angular.module('kuona.snapshot', [
+  'ui.bootstrap'
 ], function($locationProvider) {
   $locationProvider.html5Mode(true);
 });
-
-var colors = [
-  "#2484c1",
-  "#0c6197",
-  "#4daa4b",
-  "#90c469",
-  "#daca61",
-  "#e4a14b",
-  "#e98125",
-  "#cb2121",
-  "#830909",
-  "#923e99",
-  "#ae83d5",
-  "#bf273e",
-  "#ce2aeb",
-  "#bca44a",
-  "#618d1b",
-  "#1ee67b",
-  "#b0ec44",
-  "#a4a0c9",
-  "#322849",
-  "#86f71a",
-  "#d1c87f",
-  "#7d9058",
-  "#44b9b0",
-  "#7c37c0",
-  "#cc9fb1",
-  "#e65414",
-  "#8b6834",
-  "#248838"];
-
-var pieChart = function(id, title, subtitle, data) {
-  var chart = new d3pie(id,
-                        { "header": {
-                          "title": {
-                            "text": title,
-                            "fontSize": 24,
-                            "font": "open sans"
-                          },
-                          "subtitle": {
-                            "text": subtitle,
-                            "color": "#999999",
-                            "fontSize": 12,
-                            "font": "open sans"
-                          },
-                          "location": "top-left",
-                          "titleSubtitlePadding": 9
-                        },
-	                  "footer": {
-		            "color": "#999999",
-		            "fontSize": 10,
-		            "font": "open sans",
-		            "location": "bottom-left"
-	                  },
-	                  "size": {
-		            "canvasHeight": 300,
-		            "canvasWidth": 390,
-		            "pieOuterRadius": "90%"
-	                  },
-	                  "data": {
-		            "sortOrder": "value-desc",
-	                    "content": data,
-
-	                  },
-	                  "labels": {
-		            "outer": {
-			      "pieDistance": 32
-		            },
-		            "inner": {
-			      "hideWhenLessThanPercentage": 3
-		            },
-		            "mainLabel": {
-			      "fontSize": 11
-		            },
-		            "percentage": {
-			      "color": "#ffffff",
-			      "decimalPlaces": 0
-		            },
-		            "value": {
-			      "color": "#adadad",
-			      "fontSize": 11
-		            },
-		            "lines": {
-			      "enabled": true
-		            },
-		            "truncation": {
-			      "enabled": true
-		            }
-	                  },
-	                  "effects": {
-		            "pullOutSegmentOnClick": {
-			      "effect": "linear",
-			      "speed": 400,
-			      "size": 8
-		            }
-	                  },
-	                  "misc": {
-		            "gradient": {
-			      "enabled": true,
-			      "percentage": 100
-		            }
-	                  }
-                        });
-  
-};
-
-
-var polarChart = function(id, title, subtitle, data) {
-  var dataValues = [];
-  var dataColors = [];
-  var dataLabels = [];
-
-  for (var i = 0; i < data.length; i++) {
-    var item = data[i];
-    dataValues.push(item.value);
-    dataColors.push(item.color);
-    dataLabels.push(item.label);
-  }
-  
-  var polarData = {
-    datasets: [{
-      data: dataValues,
-      backgroundColor: dataColors,
-      label: [
-        title
-      ]
-    }],
-    labels: dataLabels
-  };
-
-  var polarOptions = {
-    segmentStrokeWidth: 2,
-    responsive: true
-  };
-
-  var ctx3 = document.getElementById(id).getContext("2d");
-  new Chart(ctx3, {type: 'polarArea', data: polarData, options:polarOptions});
-}
-
-var barChart = function(id, title, subtitle, data) {
-  var dataValues = [];
-  var dataColors = [];
-  var dataLabels = [];
-
-  for (var i = 0; i < data.length; i++) {
-    var item = data[i];
-    dataValues.push(item.value);
-    dataColors.push(item.color);
-    dataLabels.push(item.label);
-  }
-
-  var ctx = document.getElementById(id).getContext("2d");
-  return new Chart(ctx, {
-    type: 'horizontalBar',
-    data: { labels: dataLabels,
-            datasets: [{
-              label: title,
-              backgroundColor: dataColors[0],
-              borderColor: dataColors[0],
-              borderWidth: 1,
-              data: dataValues
-            }]},
-    options: {
-      // Elements options apply to all of the options unless overridden in a dataset
-      // In this case, we are setting the border of each horizontal bar to be 2px wide
-      elements: {
-        rectangle: {
-          borderWidth: 2,
-        }
-      },
-      responsive: true,
-      legend: {
-        position: 'right',
-      },
-      title: {
-        display: true,
-        text: subtitle
-      }
-    }
-  });
-};
 
 
 function toDependencyId(a) {
@@ -205,59 +23,6 @@ var dependencyCompare = function(a, b) {
   return (a.height - b.height) || a.id.localeCompare(b.id);
 }
 
-var dependencyTreeChart = function(treeData, treeContainer) {
-  var width = 600;
-  var height = 600;
-  var svg = treeContainer.append("svg").attr("width", width).attr("height", height);
-  var g = svg.append("g").attr("transform", "translate(150,0)");
-
-  var tree = d3.cluster()
-      .size([height, width - 400]);
-
-  var stratify = d3.stratify(treeData)
-      .id(toDependencyId)
-      .parentId(fromDependencyId);
-  
-  var root = stratify(treeData).sort(dependencyCompare);
-
-  tree(root);
-
-  var link = g.selectAll(".link")
-      .data(root.descendants().slice(1))
-      .enter().append("path")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        return "M" + d.y + "," + d.x
-          + "C" + (d.parent.y + 10) + "," + d.x
-          + " " + (d.parent.y + 10) + "," + d.parent.x
-          + " " + d.parent.y + "," + d.parent.x;
-      });
-
-  var node = g.selectAll(".node")
-      .data(root.descendants())
-      .enter().append("g")
-      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-
-  node.append("circle")
-      .attr("r", 2.5);
-
-  node.append("text")
-    .attr("dy", function(d) {
-      if (!d.parent) {
-        return -8;
-      }
-      return 2; })
-    .attr("x", function(d) {
-      if (!d.parent) {
-        return 8;
-      }
-      return d.children ? -8 : 8; })
-    .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-    .text(function(d) { return d.id; });
-//    .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
-}
-
 
 function enhanceDependencies(build) {
 
@@ -267,10 +32,6 @@ function enhanceDependencies(build) {
     if (b.dependencies != undefined && b.dependencies.dependencies != undefined) {
 
       var list = []
-
-
-      
-      // Add root to the dependency list without a from value
       list.push({"from": null, "to": b.dependencies.root});
 
       for (var k in b.dependencies.dependencies) {
@@ -279,38 +40,12 @@ function enhanceDependencies(build) {
       }
 
       b["dependencyTree"] = list;
-      
-      
-
-      
-//      var dependency_tree = {};
-//      var root = { "name": b.dependencies.root.id, "children":[ ] };
-//      dependency_tree[b.dependencies.root.id] = root;
-//      
-//      for (var k in b.dependencies.dependencies) {
-//        var item = b.dependencies.dependencies[k]
-        
-//        if (!dependency_tree[item.from.id]) {
-//          dependency_tree[item.from.id] = { "name": item.from.id, "children":[ ], "parent": null};
-//        }
-        
-//        if (!dependency_tree[item.to.id]) {
-//          dependency_tree[item.to.id] = { "name": item.to.id, "children":[ ], "parent": null };
-//        }
-        
-//        dependency_tree[item.from.id].children.push(dependency_tree[item.to.id]);
-//        dependency_tree[item.to.id].parent = item.from.id;
-//      }
-      
-//      b["dependencyTree"] = root;
     }
   }
 }
 
 
 function SnapshotController($scope, $http, $location) {
-  console.log("snapshot controller");
-
   $scope.id = $location.search().id;
   $scope.repository = {}
   $scope.snapshot = {};
@@ -323,28 +58,21 @@ function SnapshotController($scope, $http, $location) {
     
     for (var i = 0; i < $scope.snapshot.content.file_details.length; i++) {
       var item = $scope.snapshot.content.file_details[i];
-      $scope.file_piechart_data.push({
-	"label": item.language,
-        "color": colors[i],
-	"value": item.count
+      $scope.file_piechart_data.push({ "label": item.language, "color": colors[i], "value": item.count
       });
     }
 
-    barChart("filesBarCanvas", "Files", "Repository files by type", $scope.file_piechart_data);
+    barChart(document.getElementById("filesBarCanvas"), "Files", "Repository files by type", $scope.file_piechart_data);
 
     $scope.code_piechart_data = [];
     for (var i = 0; i < $scope.snapshot.content.code_line_details.length; i++) {
       var item = $scope.snapshot.content.code_line_details[i];
       console.log(item);
-      $scope.code_piechart_data.push({
-	"label": item.language,
-        "color": colors[i],
-	"value": item.count
+      $scope.code_piechart_data.push({"label": item.language, "color": colors[i], "value": item.count
       });
     }
 
-    barChart("codeBarCanvas", "Code", "Lines of code by type", $scope.code_piechart_data);
-
+    barChart(document.getElementById("codeBarCanvas"), "Code", "Lines of code by type", $scope.code_piechart_data);
 
     enhanceDependencies($scope.snapshot.build);
   });
@@ -353,11 +81,11 @@ function SnapshotController($scope, $http, $location) {
     $scope.repository = res.data;
     $scope.avatar_url = $scope.repository.project.owner.avatar_url
   });
-};
+}
 
-kuona_snapshot.controller('SnapshotController',['$scope', '$http', '$location',  SnapshotController]);
+kuonaSnapshot.controller('SnapshotController',['$scope', '$http', '$location',  SnapshotController]);
 
-kuona_snapshot.directive('dependencyChart', function () {
+kuonaSnapshot.directive('dependencyChart', function () {
     return {
         restrict: 'E',
         scope: {
