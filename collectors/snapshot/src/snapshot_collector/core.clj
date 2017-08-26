0335d49001c648b678ace9318f4aad5c49749b4b
Graham Brooks <graham@grahambrooks.com>
2017-08-26 16:33:00 -0600

    Added more tests and refactored collector into separate namespaces

--- collectors/snapshot/src/snapshot_collector/core.clj
+++ collectors/snapshot/src/snapshot_collector/core.clj
@@ -2,19 +2,7 @@
   (:require [clojure.string :as string]
             [clojure.tools.logging :as log]
             [clojure.tools.cli :refer [parse-opts]]
-            [clojure.java.io :as io]
-            [clj-jgit.porcelain :as git]
-            [clj-jgit.querying :as git-query]
-            [clojure.java.shell :as shell]
-            [cheshire.core :refer :all]
-            [slingshot.slingshot :refer :all]
-            [kuona-core.metric.store :as store]
-            [kuona-core.git :refer :all]
-            [kuona-core.util :refer :all]
-            [kuona-core.cloc :as cloc]
-            [kuona-core.builder :as builder]
-            [clj-http.client :as http])
-  (:import (java.net InetAddress))
+            [snapshot-collector.snapshot :as snapshot])
   (:gen-class))
 
 (def cli-options
@@ -33,7 +21,6 @@
         options-summary
         ""]
        (string/join \newline)))
-
 
 (defn error-msg [errors]
   (str "The following errors occurred while parsing your command:\n\n"
@@ -54,144 +41,10 @@
   (println msg)
   (System/exit status))
 
-
-(defn get-repositories
-  [url]
-  (parse-json-body (http/get url)))
-
-(defn repository-id
-  [r]
-  (:id r))
-
-(defn repository-git-url
-  [r]
-  (-> r :url))
-
-(defn project-metrics
-  [project]
-  {:name              (:name project)
-   :description       (:description project)
-   :open_issues_count (:open_issues_count project)
-   :watchers_count    (:watchers_count project)
-   :forks_count       (:forks_count project)
-   :stargazers_count  (:stargazers_count project)
-   :size              (:size project)
-   :updated_at        (:updated_at project)
-   :language          (:language project)
-   :pushed_at         (:pushed_at project)
-   :owner_avatar_url  (:owner :avatar_url)
-   })
-
-(defn create-snapshot
-  [project loc-data builder-data]
-  {:repository (project-metrics project)
-   :content    loc-data
-   :build      builder-data})
-
-(defn put-snapshot
-  [snapshot id]
-  (let [url (string/join "/" ["http://dashboard.kuona.io/api/snapshots" id])]
-    (log/info "put-snapshot " url)
-    (parse-json-body (http/put url
-                               {:headers {"content-type" "application/json; charset=UTF-8"}
-                                :body    (generate-string snapshot)}))))
-
-(defn put-commit!
-  [repo-id commit]
-  (let [url (string/join "/" ["http://dashboard.kuona.io/api/repositories" repo-id "commits"])]
-    (log/info "put-commit " url)
-    (parse-json-body (http/put url
-                               {:headers {"content-type" "application/json; charset=UTF-8"}
-                                :body    (generate-string commit)}))))
-
-(defn has-snapshot?
-  [id]
-  (let [url (string/join "/" ["http://dashboard.kuona.io/api/snapshots" id])]
-    (try+
-      (log/info "has-snapshot? " url)
-      (http/get url)
-      true
-      (catch Object _
-        false))))
-
-(defn requires-snapshot?
-  [id]
-  (not (has-snapshot? id)))
-
-(defn language-x-count
-  [item k]
-  ;  (log/info "language-x-count " item k)
-  {:language (:language item) :count (k item)})
-
-(defn loc-metrics
-  [loc-data]
-  (let [content (-> loc-data :metric :activity)]
-    {:file_count           (:file-count content)
-     :comment_lines        (:comment-lines content)
-     :blank_lines          (:blank-lines content)
-     :code_lines           (:code-lines content)
-     :file_details         (into [] (map #(language-x-count % :file-count) (:languages content)))
-     :blank_line_details   (into [] (map #(language-x-count % :blank-lines) (:languages content)))
-     :comment_line_details (into [] (map #(language-x-count % :comment-lines) (:languages content)))
-     :code_line_details    (into [] (map #(language-x-count % :code-lines) (:languages content)))
-     }))
-
-(defn snapshot-repository
-  [repo]
-  (try+
-    (let [id (repository-id repo)
-          url (repository-git-url repo)
-          local-dir (canonical-path (string/join "/" ["/Volumes/data-drive/workspace" id]))
-          name (-> repo :project :name)]
-      (log/info "Snapshotting " id name "from " url "to " local-dir)
-      (if (directory? local-dir) (git-pull url local-dir) (git-clone url local-dir))
-      (let [loc-data (cloc/loc-collector (fn [a] a) local-dir "foo")
-            build-data (builder/collect-builder-metrics local-dir)
-            snapshot-data (create-snapshot (-> repo :project) (loc-metrics loc-data) build-data)]
-        (doall (map #(put-commit! id %) (commit-history (git/load-repo local-dir))))
-        (log/info "snapshot " (put-snapshot snapshot-data id))
-        ))
-    (catch Object _
-      (log/error (:throwable &throw-context) "Unexpected error"))))
-
-(defn requires-snapshot-filter
-  [repo]
-  (let [id (repository-id repo)]
-    (requires-snapshot? id)))
-
-(defn all-repositories
-  ([api-uri] (all-repositories api-uri 1))
-  ([api-uri page]
-   (do
-     (log/info "Reading repository page " page)
-     (let [result (get-repositories (str api-uri "/api/repositories?page=" page))
-           count (-> result :count)
-           items (-> result :items)]
-       (if (= count 0) items (concat items (all-repositories api-uri (inc page))))))))
-
-
-(defn- snapshot-repositories
-  [repositories]
-  (log/info "Creating snapshots for " (count repositories) " repositories")
-  (doall (map snapshot-repository repositories)))
-
-(defn run-snapshot
-  [options]
-  (let [api-url (:api-url options)
-        workspace (:workspace options)
-        force-update (:force options)
-        update-filter (fn [r] (if force-update true (requires-snapshot-filter r)))]
-    (log/info "Updating " api-url " using " workspace " for repository data")
-    (let [repositories (all-repositories api-url)]
-      (log/info "Found " (count repositories) " configured repositories for analysis")
-      (snapshot-repositories (filter update-filter repositories)))
-    ))
-
 (defn -main
   [& args]
   (log/info "Kuona Snapshot Collector")
   (let [options (validate-args args)]
     (cond
       (contains? options :exit-message) (exit (if (:ok? options) 0 1) (:exit-message options))
-      :else (run-snapshot options)
-      )))
+      :else (snapshot/run options))))
---
+++ collectors/snapshot/src/snapshot_collector/snapshot.clj
@@ -0,0 +1,153 @@
+(ns snapshot-collector.snapshot
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
+            [clojure.java.io :as io]
+            [clj-jgit.porcelain :as git]
+            [clj-jgit.querying :as git-query]
+            [clojure.java.shell :as shell]
+            [cheshire.core :refer :all]
+            [slingshot.slingshot :refer :all]
+            [kuona-core.metric.store :as store]
+            [kuona-core.git :refer :all]
+            [kuona-core.util :refer :all]
+            [kuona-core.cloc :as cloc]
+            [kuona-core.builder :as builder]
+            [clj-http.client :as http])
+  (:gen-class))
+
+(defn get-repositories
+  [url]
+  (parse-json-body (http/get url)))
+
+(defn repository-id
+  [r]
+  (:id r))
+
+(defn repository-git-url
+  [r]
+  (-> r :url))
+
+(defn project-metrics
+  [project]
+  {:name              (:name project)
+   :description       (:description project)
+   :open_issues_count (:open_issues_count project)
+   :watchers_count    (:watchers_count project)
+   :forks_count       (:forks_count project)
+   :stargazers_count  (:stargazers_count project)
+   :size              (:size project)
+   :updated_at        (:updated_at project)
+   :language          (:language project)
+   :pushed_at         (:pushed_at project)
+   :owner_avatar_url  (:avatar_url (:owner project))
+   })
+
+(defn create-snapshot
+  [project loc-data builder-data]
+  {:repository (project-metrics project)
+   :content    loc-data
+   :build      builder-data})
+
+(defn snapshot-url
+  [api-url id]
+  (string/join "/" [api-url "api/snapshots" id]))
+
+(defn put-snapshot!
+  [snapshot url]
+  (log/info "put-snapshot " url)
+  (let [request {:headers {"content-type" "application/json; charset=UTF-8"}
+                 :body    (generate-string snapshot)}]
+    (parse-json-body (http/put url request))))
+
+(defn snapshot-commits-url
+  [api-url id]
+  (string/join "/" [(snapshot-url api-url id) "commits"]))
+
+(defn put-commit!
+  [commit url]
+  (log/info "put-commit " url)
+  (let [request {:headers {"content-type" "application/json; charset=UTF-8"}
+                 :body    (generate-string commit)}]
+    (parse-json-body (http/put url request))))
+
+(defn has-snapshot?
+  [id]
+  (let [url (string/join "/" ["http://dashboard.kuona.io/api/snapshots" id])]
+    (try+
+      (log/info "has-snapshot? " url)
+      (http/get url)
+      true
+      (catch Object _
+        false))))
+
+(defn language-x-count
+  [item k]
+  {:language (:language item) :count (k item)})
+
+(defn loc-metrics
+  [loc-data]
+  (let [content (-> loc-data :metric :activity)]
+    {:file_count           (:file-count content)
+     :comment_lines        (:comment-lines content)
+     :blank_lines          (:blank-lines content)
+     :code_lines           (:code-lines content)
+     :file_details         (into [] (map #(language-x-count % :file-count) (:languages content)))
+     :blank_line_details   (into [] (map #(language-x-count % :blank-lines) (:languages content)))
+     :comment_line_details (into [] (map #(language-x-count % :comment-lines) (:languages content)))
+     :code_line_details    (into [] (map #(language-x-count % :code-lines) (:languages content)))
+     }))
+
+(defn- clone-or-update
+  [url local-dir]
+  (if (directory? local-dir) (git-pull url local-dir) (git-clone url local-dir)))
+
+(defn create-repository-snapshot
+  [api-url workspace repo]
+  (try+
+    (let [id (repository-id repo)
+          url (repository-git-url repo)
+          local-dir (canonical-path (string/join "/" [workspace id]))
+          name (-> repo :project :name)]
+      (log/info "Creating repository snapshot " id name "from " url "to " local-dir)
+      (clone-or-update url local-dir)
+      (let [loc-data (cloc/loc-collector (fn [a] a) local-dir "foo")
+            build-data (builder/collect-builder-metrics local-dir)
+            snapshot-data (create-snapshot (-> repo :project) (loc-metrics loc-data) build-data)]
+        (doall (map #(put-commit! % (snapshot-commits-url api-url id)) (commit-history (git/load-repo local-dir))))
+        (log/info "snapshot " (put-snapshot! snapshot-data (snapshot-url api-url id)))
+        ))
+    (catch Object _
+      (log/error (:throwable &throw-context) "Unexpected error"))))
+
+(defn requires-snapshot?
+  [repo]
+  (let [id (repository-id repo)]
+    (not (has-snapshot? id))))
+
+(defn all-repositories
+  ([api-uri] (all-repositories api-uri 1))
+  ([api-uri page]
+   (do
+     (log/info "Reading repository page " page)
+     (let [result (get-repositories (str api-uri "/api/repositories?page=" page))
+           count (-> result :count)
+           items (-> result :items)]
+       (if (= count 0) items (concat items (all-repositories api-uri (inc page))))))))
+
+(defn- create-snapshots
+  [api-url workspace repositories]
+  (log/info "Creating snapshots for " (count repositories) " repositories")
+  (doall (map (fn [repo] (create-repository-snapshot api-url workspace repo)) repositories)))
+
+(defn run
+  "Main entry point for snapshot collection."
+  [options]
+  (let [api-url (:api-url options)
+        workspace (:workspace options)
+        force-update (:force options)
+        requires-update (fn [r] (if force-update true (requires-snapshot? r)))]
+    (log/info "Updating " api-url " using " workspace " for repository data")
+    (let [repositories (all-repositories api-url)]
+      (log/info "Found " (count repositories) " configured repositories for analysis")
+      (create-snapshots api-url workspace (filter requires-update repositories)))
+    ))
