468fa2cd9a148e87d7817217bb80693f7120dd0c
Graham Brooks <graham@grahambrooks.com>
2017-09-17 18:09:17 -0600

    Fixed broken tests

--- collectors/snapshot/test/snapshot_collector/core_test.clj
+++ collectors/snapshot/test/snapshot_collector/core_test.clj
@@ -1,24 +1,25 @@
 (ns snapshot-collector.core-test
   (:require [midje.sweet :refer :all]
+            [kuona-core.cli :as cli]
             [snapshot-collector.core :refer :all]))
 
 
 (facts "about command line interface"
        (fact "uses defaults if no arguments supplied"
-             (validate-args []) => {:api-url   "http://dashboard.kuona.io"
-                                    :force     false
-                                    :workspace "/Volumes/data-drive/workspace"})
+             (cli/validate-args "snap" cli-options []) => {:api-url   "http://dashboard.kuona.io"
+                                                           :force     false
+                                                           :workspace "/Volumes/data-drive/workspace"})
        (fact "can supply short parameters"
-             (validate-args ["-a" "http://localhost" "-f" "-w" "/some/path"]) => {:api-url   "http://localhost"
-                                                                                  :force     true
-                                                                                  :workspace "/some/path"})
+             (cli/validate-args "snap" cli-options  ["-a" "http://localhost" "-f" "-w" "/some/path"]) => {:api-url   "http://localhost"
+                                                                                                          :force     true
+                                                                                                          :workspace "/some/path"})
        (fact "can supply long parameters"
-             (validate-args ["--api-url" "http://localhost" "--force" "--workspace" "/some/path"]) => {:api-url   "http://localhost"
-                                                                                                       :force     true
-                                                                                                       :workspace "/some/path"})
+             (cli/validate-args "snap" cli-options ["--api-url" "http://localhost" "--force" "--workspace" "/some/path"]) => {:api-url   "http://localhost"
+                                                                                                                              :force     true
+                                                                                                                              :workspace "/some/path"})
        (fact "invalid options produce error"
-             (validate-args ["--foo"]) => {:exit-message "The following errors occurred while parsing your command:\n\nUnknown option: \"--foo\""}
-             (:ok? (validate-args ["--foo"])) => nil)
+             (cli/validate-args "snap" cli-options ["--foo"]) => {:exit-message "The following errors occurred while parsing your command:\n\nUnknown option: \"--foo\""}
+             (:ok? (cli/validate-args "snap" cli-options ["--foo"])) => nil)
        (fact "help support"
-             (:ok? (validate-args ["-h"])) => true
-             (contains? (validate-args ["-h"]) :exit-message) => true))
+             (:ok? (cli/validate-args "snap" cli-options ["-h"])) => true
+             (contains? (cli/validate-args "snap" cli-options ["-h"]) :exit-message) => true))
