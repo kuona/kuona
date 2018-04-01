5a41d7ba756cc5f68b37b9c23a083de903d9537f
Graham Brooks <graham@grahambrooks.com>
2018-04-01 07:59:31 -0600

    added query of API

--- tools/tfs-crawler/src/tfs_crawler/crawler.clj
+++ tools/tfs-crawler/src/tfs_crawler/crawler.clj
@@ -2,11 +2,25 @@
   (:require [clojure.tools.logging :as log]
             [clj-http.client :as http]
             [cheshire.core :refer :all]
-            [slingshot.slingshot :refer :all])
+            [slingshot.slingshot :refer :all]
+            [kuona-core.util :as util])
   (:gen-class))
 
 (defn vs-url [org]
   (str "https://" org ".visualstudio.com/_apis/git/repositories?api-version=4.1"))
+
+
+(defn tfs-to-repository-entry
+  [item]
+  {:source          :tfs
+   :github_language nil
+   :url             (-> item :sshUrl)
+   :project         item
+   :last_analysed   nil})
+
+(defn read-available-repositories
+  [url, token]
+  (util/parse-json-body (http/get url {:basic-auth ["" token]})))
 
 (defn crawl
   [config]
--- tools/tfs-crawler/test/tfs_crawler/crawler_test.clj
+++ tools/tfs-crawler/test/tfs_crawler/crawler_test.clj
@@ -1,8 +1,23 @@
 (ns tfs-crawler.crawler-test
   (:require [midje.sweet :refer :all]
-            [tfs-crawler.crawler :as crawler]))
+            [cheshire.core :refer :all]
+            [tfs-crawler.crawler :as crawler]
+            [clj-http.client :as http]))
 
 (facts "about visual studio API urls"
        (fact "contains the organisation name"
              (crawler/vs-url "foo") => "https://foo.visualstudio.com/_apis/git/repositories?api-version=4.1"))
 
+
+(facts "about repository data conversation"
+       (fact
+         (crawler/tfs-to-repository-entry {:sshUrl "foo"}) => {:github_language nil
+                                                               :last_analysed   nil
+                                                               :project         {:sshUrl "foo"}
+                                                               :source          :tfs
+                                                               :url             "foo"}))
+
+(facts "about reading repositories"
+       (fact
+         (crawler/read-available-repositories "http://vs" "token") => {:count 0}
+         (provided (http/get "http://vs" {:basic-auth ["" "token"]}) => {:body (generate-string {:count 0})})))
