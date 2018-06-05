a366fedf6154ab5c913e2b0d2c793d101396b0be
Graham Brooks <graham@grahambrooks.com>
2018-06-04 21:55:06 -0600

    Added javascript driven (vis.js) manifest display

--- dashboard/src/js/snapshot.js
+++ dashboard/src/js/snapshot.js
@@ -44,6 +44,38 @@ function enhanceDependencies(build) {
   }
 }
 
+function manifestNodeShape(t) {
+  switch (t) {
+    case 'database':
+      return 'database';
+    default:
+      return 'box';
+  }
+}
+
+function manifestGraph(data) {
+  console.log(data);
+  var result = {nodes: [], edges: []};
+  var seen = {};
+  for (var i in data.components) {
+    var c = data.components[i]
+    console.log(c.id);
+    if (!(c.id in seen)) {
+      result.nodes.push({id: c.id, label: c.description, shape: manifestNodeShape(c.kind)});
+      seen[c.id] = true;
+    }
+    for (var j in c.dependencies) {
+      var d = c.dependencies[j]
+      if (!(d.id in seen)) {
+        result.nodes.push({id: d.id, label: d.id, shape: manifestNodeShape(d.kind)});
+        seen[d.id] = true;
+      }
+      result.edges.push({from: c.id, to: d.id});
+    }
+  }
+
+  return result;
+}
 
 function SnapshotController($scope, $http, $location) {
   $scope.id = $location.search().id;
@@ -161,67 +193,37 @@ kuonaSnapshot.directive('artifactPanel', function () {
   };
 });
 
+kuonaSnapshot.directive('manifestPanel', function () {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      data: '='
+    },
+    link: function (scope, element, attrs) {
+      scope.$watch('data', function (newValue, oldValue) {
+        if (scope.data) {
+          var graphData = manifestGraph(scope.data);
+          console.log(scope.data);
+          new vis.Network(element[0],
+            {
+              nodes: new vis.DataSet(graphData.nodes),
+              edges: new vis.DataSet(graphData.edges)
+            }, {
+              autoResize: true,
+              height: '300px',
+              width: '100%',
+              nodes: {fixed: true},
+              edges: {arrows: 'to'},
+              layout: {
+                hierarchical: {direction: 'UD'}
+              }
+            });
+        } else {
+          console.warn("manifest graph directive called with no data");
+        }
+      });
+    }
+  };
 
-// Create a new directed graph
-var g = new dagreD3.graphlib.Graph().setGraph({});
-
-// States and transitions from RFC 793
-var states = ["CLOSED", "LISTEN", "SYN RCVD", "SYN SENT",
-  "ESTAB", "FINWAIT-1", "CLOSE WAIT", "FINWAIT-2",
-  "CLOSING", "LAST-ACK", "TIME WAIT"];
-
-// Automatically label each of the nodes
-states.forEach(function (state) {
-  g.setNode(state, {label: state});
 });
-
-// Set up the edges
-g.setEdge("CLOSED", "LISTEN", {label: "open"});
-g.setEdge("LISTEN", "SYN RCVD", {label: "rcv SYN"});
-g.setEdge("LISTEN", "SYN SENT", {label: "send"});
-g.setEdge("LISTEN", "CLOSED", {label: "close"});
-g.setEdge("SYN RCVD", "FINWAIT-1", {label: "close"});
-g.setEdge("SYN RCVD", "ESTAB", {label: "rcv ACK of SYN"});
-g.setEdge("SYN SENT", "SYN RCVD", {label: "rcv SYN"});
-g.setEdge("SYN SENT", "ESTAB", {label: "rcv SYN, ACK"});
-g.setEdge("SYN SENT", "CLOSED", {label: "close"});
-g.setEdge("ESTAB", "FINWAIT-1", {label: "close"});
-g.setEdge("ESTAB", "CLOSE WAIT", {label: "rcv FIN"});
-g.setEdge("FINWAIT-1", "FINWAIT-2", {label: "rcv ACK of FIN"});
-g.setEdge("FINWAIT-1", "CLOSING", {label: "rcv FIN"});
-g.setEdge("CLOSE WAIT", "LAST-ACK", {label: "close"});
-g.setEdge("FINWAIT-2", "TIME WAIT", {label: "rcv FIN"});
-g.setEdge("CLOSING", "TIME WAIT", {label: "rcv ACK of FIN"});
-g.setEdge("LAST-ACK", "CLOSED", {label: "rcv ACK of FIN"});
-g.setEdge("TIME WAIT", "CLOSED", {label: "timeout=2MSL"});
-
-// Set some general styles
-g.nodes().forEach(function (v) {
-  var node = g.node(v);
-  node.rx = node.ry = 5;
-});
-
-// Add some custom colors based on state
-g.node('CLOSED').style = "fill: #f77";
-g.node('ESTAB').style = "fill: #7f7";
-
-var svg = d3.select("svg"),
-  inner = svg.select("g");
-
-// Set up zoom support
-var zoom = d3.zoom().on("zoom", function () {
-  inner.attr("transform", d3.event.transform);
-});
-svg.call(zoom);
-
-// Create the renderer
-var render = new dagreD3.render();
-
-// Run the renderer. This is what draws the final graph.
-render(inner, g);
-
-// Center the graph
-var initialScale = 2.00;
-svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr("width") - g.graph().width * initialScale) / 2, 20).scale(initialScale));
-
-svg.attr('height', g.graph().height * initialScale + 40);
