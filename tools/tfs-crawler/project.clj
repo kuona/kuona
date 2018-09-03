f0ed0f9e02189dfde3817ad9491fe7ad16c5f852
Graham Brooks <graham@grahambrooks.com>
2018-09-03 10:51:54 -0600

    Updated dependencies for the TFS crawler

--- tools/tfs-crawler/project.clj
+++ tools/tfs-crawler/project.clj
@@ -5,17 +5,17 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [com.jcabi/jcabi-log "0.18"]
-                 [clj-http "3.8.0"]
+                 [clj-http "3.9.1"]
                  [cheshire "5.8.0"]
-                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/tools.cli "0.3.7"]
                  [kuona-core "0.0.2"]]
   :plugins [[lein-midje "3.2.1"]
             [lein-ancient "0.6.14"]]
-  :profiles {:dev     {:dependencies [[midje "1.9.1"]]}
+  :profiles {:dev     {:dependencies [[midje "1.9.2"]]}
              :uberjar {:aot :all}})
