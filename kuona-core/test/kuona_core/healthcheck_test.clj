5f4cf33c29a3eebf0a10a8b9801dd18d38291d31
Graham Brooks <graham@grahambrooks.com>
2018-10-02 17:40:55 -0400

    Separaated out the healthcheck namespace ready for use

--- kuona-core/test/kuona_core/healthcheck_test.clj
+++ kuona-core/test/kuona_core/healthcheck_test.clj
@@ -1,27 +1,8 @@
 (ns kuona-core.healthcheck-test
   (:require [midje.sweet :refer :all]
-            [kuona-core.http :as http]))
+            [kuona-core.http :as http]
+            [kuona-core.healthcheck :refer :all]))
 
-(defn filter-actuator-links
-  "Filters the list of actuator links for health and info references"
-  [links]
-  (merge {} (-> links
-                :_links
-                (select-keys [:info :health])))
-  )
-(defn merge-list
-  "Merge the maps in the list into a single map"
-  [list]
-  (if (first list)
-    (merge (first list) (merge-list (rest list)))
-    {}))
-
-(defn- read-link
-  "reads the json response from the :href link"
-  [link]
-  (if (:href link)
-    (http/json-get (:href link))
-    {}))
 
 (facts "about merging lists"
        (fact (merge-list []) => {})
@@ -29,14 +10,6 @@
        (fact (merge-list [{:foo :bar}]) => {:foo :bar})
        (fact (merge-list `({:foo :bar})) => {:foo :bar})
        (fact (merge-list [{:foo 1} {:bar 2}]) => {:foo 1 :bar 2}))
-
-
-(defn spring-actuator-health
-  "Given a Spring actuator endpoint returns the health and info endpoints as a single map."
-  [url]
-  (let [actuator-links (http/json-get url)
-        links          (filter-actuator-links actuator-links)]
-    (merge-list (map read-link (vals links)))))
 
 (facts "about spring actuator filter"
        (fact (filter-actuator-links {}) => {})
@@ -50,11 +23,13 @@
              (provided (http/json-get "http://some.health.url") => "result")))
 
 (facts "spring actuator health check checks"
+       (fact "empty map if health and info are missing"
+             (spring-actuator-health "http://some.url") => {}
+             (provided (http/json-get "http://some.url") => {:_links {}}))
        (fact "result includes status"
              (spring-actuator-health "http://some.url") => {:status "DOWN"}
              (provided (http/json-get "http://some.url") => {:_links {:health {:href "http://some.health.url"}}})
              (provided (http/json-get "http://some.health.url") => {:status "DOWN"}))
-
        (fact "result includes status and info"
              (spring-actuator-health "http://some.url") => {:status "DOWN"
                                                             :info   "INFO"}
