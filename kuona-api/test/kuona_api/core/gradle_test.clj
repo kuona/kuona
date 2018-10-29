1064fafbe525247d34f8b01d9ef31e3cc419f2a7
Graham Brooks <graham@grahambrooks.com>
2018-10-29 18:50:30 -0400

    Improved test to be less JDK version dependent

--- kuona-api/test/kuona_api/core/gradle_test.clj
+++ kuona-api/test/kuona_api/core/gradle_test.clj
@@ -1,37 +1,36 @@
 (ns kuona-api.core.gradle-test
-  (:require [midje.sweet :refer :all]
-            [kuona-api.core.gradle :as gradle])
-  )
+    (:require [midje.sweet :refer :all]
+      [kuona-api.core.gradle :as gradle])
+    )
 
 
 (facts "about gradle builds"
        (let [test-project-path "../test/gradle-dependencies"]
-         (fact
-           (:project (gradle/analyse-gradle-project test-project-path)) => (contains {:description nil}))
-         (fact
-           (gradle/analyse-gradle-project test-project-path)
-           => {:project {:description nil
-                         :jdk         "1.10"
-                         :modules     [{:dependencies [{:exported false
-                                                        :group    "org.antlr"
-                                                        :name     "antlr4-runtime"
-                                                        :scope    "RUNTIME"
-                                                        :version  "4.7"}
-                                                       {:exported false
-                                                        :group    "junit"
-                                                        :name     "junit"
-                                                        :scope    "TEST"
-                                                        :version  "4.12"}
-                                                       {:exported false
-                                                        :group    "org.hamcrest"
-                                                        :name     "hamcrest-core"
-                                                        :scope    "TEST"
-                                                        :version  "1.3"}
-                                                       {:exported false
-                                                        :group    "org.antlr"
-                                                        :name     "antlr4-runtime"
-                                                        :scope    "TEST"
-                                                        :version  "4.7"}]
-                                        :description  nil
-                                        :name         "gradle-dependencies"}]
-                         :name        "gradle-dependencies"}})))
+            (fact
+              (:project (gradle/analyse-gradle-project test-project-path)) => (contains {:description nil}))
+            (fact
+              (let [record (gradle/analyse-gradle-project test-project-path)
+                    project (-> record :project)]
+                   project => (contains {:modules [{:dependencies [{:exported false
+                                                                    :group    "org.antlr"
+                                                                    :name     "antlr4-runtime"
+                                                                    :scope    "RUNTIME"
+                                                                    :version  "4.7"}
+                                                                   {:exported false
+                                                                    :group    "junit"
+                                                                    :name     "junit"
+                                                                    :scope    "TEST"
+                                                                    :version  "4.12"}
+                                                                   {:exported false
+                                                                    :group    "org.hamcrest"
+                                                                    :name     "hamcrest-core"
+                                                                    :scope    "TEST"
+                                                                    :version  "1.3"}
+                                                                   {:exported false
+                                                                    :group    "org.antlr"
+                                                                    :name     "antlr4-runtime"
+                                                                    :scope    "TEST"
+                                                                    :version  "4.7"}]
+                                                    :description  nil
+                                                    :name         "gradle-dependencies"}]})
+                   ))))
