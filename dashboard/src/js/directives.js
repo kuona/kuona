0225839f05f454c36aa4913ab93a8a80609f20bf
Graham Brooks <graham@grahambrooks.com>
2018-11-11 10:26:41 -0700

    Moved dashboard angular directives into a separate file and added a wrapper function to register them all in one shot

--- dashboard/src/js/directives.js
+++ dashboard/src/js/directives.js
@@ -18,3 +18,80 @@ function pageTitle($rootScope, $timeout) {
   }
 }
 
+function registerModuleDirectives(module) {
+
+  module.directive('dashboardPanel', function () {
+    return {
+      restrict: 'E',
+      link: function (scope, element, attrs) {
+        scope.contentUrl = '/directives/' + attrs.type + '-panel.html';
+        scope.build = attrs.data;
+        attrs.$observe("ver", function (v) {
+          scope.contentUrl = '/directives/' + v + '-panel.html';
+          scope.build = attrs.data;
+        });
+      },
+      template: '<div ng-include="contentUrl"></div>'
+    };
+  });
+
+  module.directive('buildStatusPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        build: "=data"
+      },
+      templateUrl: '/directives/build-status-panel.html'
+    };
+  });
+
+  module.directive('countMetricPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        metric: "=data"
+      },
+      templateUrl: '/directives/count-metric-panel.html'
+    };
+  });
+
+  module.directive('pieChartPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: "=data"
+      },
+      templateUrl: '/directives/pie-chart-panel.html'
+    };
+  });
+
+  module.directive('barChartPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: "=data"
+      },
+      templateUrl: '/directives/bar-chart-panel.html'
+    };
+  });
+
+  module.directive('activityFeedPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: "=data"
+      },
+      templateUrl: '/directives/activity-feed-panel.html'
+    };
+  });
+  module.directive('healthCheckPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: "=data"
+      },
+      templateUrl: '/directives/health-check-panel.html'
+    };
+  });
+
+}
